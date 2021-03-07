6feb082b416dff52c9a39267eda98af70409129c
andrei <git1@email.com>
2021-03-07 17:02:41 +0100

    (1) Retrofit Tutorial Part 4
    - PUT, PATCH & DELETE REQUEST - Android Studio Tutorial

--- app/src/main/java/com/example/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/example/retrofittutorial/MainActivity.java
@@ -37,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         // getPosts();
         // getComments();
-        createPost();
+        // createPost();
+        updatePost();
     }
 
     private void getPosts() {
@@ -119,6 +120,39 @@ public class MainActivity extends AppCompatActivity {
         fields.put("title", "New Title");
 
         Call<Post> call = jsonPlaceHolderApi.createPost(fields);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void updatePost() {
+        Post post = new Post(12, null, "New Text");
+
+        Call<Post> call = jsonPlaceHolderApi.putPost(5, post);
 
         call.enqueue(new Callback<Post>() {
             @Override
