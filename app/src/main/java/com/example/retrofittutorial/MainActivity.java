24e9ab733eba5a69301d75a9d9b90f93f48d5423
andrei <git1@email.com>
2021-03-09 16:53:41 +0100

    (3) Retrofit Tutorial Part 6

--- app/src/main/java/com/example/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/example/retrofittutorial/MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+                        Request originalRequest = chain.request();
+
+                        Request newRequest = originalRequest.newBuilder()
+                                .header("Interceptor-Header", "xyz")
+                                .build();
+
+                        return chain.proceed(newRequest);
+                    }
+                })
                 .addInterceptor(loggingInterceptor)
                 .build();
 
@@ -50,10 +65,10 @@ public class MainActivity extends AppCompatActivity {
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        // getPosts();
+        getPosts();
         // getComments();
         // createPost();
-        updatePost();
+        // updatePost();
         // deletePost();
     }
 
